c8ac0604c04473750e2461330d6226fc10c25122
¨Sébastien <lehmstedt-sebastien@hotmail.fr>
2024-12-01 19:59:29 -0500

    test multiple tariffs

--- src/domain/useCases/setTariff.spec.ts
+++ src/domain/useCases/setTariff.spec.ts
@@ -8,7 +8,7 @@ import { Resource } from "@/domain/entities/Resource";
 
 describe('setTariff', () => {
 
-    it('should throw if country is not existing', async() => {
+    it('should throw if country is not existing', async () => {
 
         const countryRepository = new InMemoryCountryRepository()
         const resourceRepository = new InMemoryResourceRepository()
@@ -18,7 +18,7 @@ describe('setTariff', () => {
 
     })
 
-    it('should throw if resource is not existing', async() => {
+    it('should throw if resource is not existing', async () => {
 
         const countryRepository = new InMemoryCountryRepository()
         const country = new Country()
@@ -30,12 +30,12 @@ describe('setTariff', () => {
 
     })
 
-    it('should set a tariff of 15 percent for importing Wine', async() => {
+    it('should set a tariff of 15 percent for importing Wine', async () => {
 
         const countryRepository = new InMemoryCountryRepository()
         const resourceRepository = new InMemoryResourceRepository()
 
-        const country = new Country() 
+        const country = new Country()
         await countryRepository.save(country)
 
         const resource = new Resource('Wine')
@@ -45,10 +45,30 @@ describe('setTariff', () => {
         await setTariff.execute(country.id, 15, resource.name)
 
         const countryAfter = await countryRepository.getById(country.id) as Country
-        expect(countryAfter.id).toEqual(country.id)
-        expect(countryAfter).toEqual(country)
 
         expect(countryAfter.getTariffOnResource(resource)).toEqual(15)
 
+    })
+
+    it('should add a 10 percent tariff on Cheese when country has already a tariff on Wine', async () => {
+        const countryRepository = new InMemoryCountryRepository()
+        const resourceRepository = new InMemoryResourceRepository()
+
+        const wine = new Resource('Wine')
+        const cheese = new Resource('Cheese')
+        await resourceRepository.add(wine)
+        await resourceRepository.add(cheese)
+
+        const country = new Country()
+        country.setTariff(15, wine)
+        await countryRepository.save(country)
+
+        const setTariff = new SetTariff(countryRepository, resourceRepository)
+        await setTariff.execute(country.id, 10, cheese.name)
+
+        const countryAfter = await countryRepository.getById(country.id) as Country
+
+        expect(countryAfter.getTariffOnResource(wine)).toEqual(15)
+        expect(countryAfter.getTariffOnResource(cheese)).toEqual(10)
     })
 }) 

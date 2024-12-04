5231f113e3c3f8d975694bccc2d7c56779f0413b
¨Sébastien <lehmstedt-sebastien@hotmail.fr>
2024-12-04 08:41:59 -0500

    List empty tariffs

--- src/domain/useCases/ListTariffs.spec.ts
+++ src/domain/useCases/ListTariffs.spec.ts
@@ -1,12 +1,27 @@
 import { describe, expect, it } from "vitest";
 import { CountryNotFoundError } from "@/domain/Errors";
 import { ListTariffs } from "@/domain/useCases/ListTariffs";
+import { InMemoryCountryRepository } from "@/infrastructure/InMemoryCountryRepository";
+import { Country, CountryId } from "@/domain/entities/Country";
 
 describe("ListTariffs", () => {
     it("should throw when country is not existing", async () => {
+        const countryRepository = new InMemoryCountryRepository()
+        const listTariffs = new ListTariffs(countryRepository)
+        await expect(() => listTariffs.execute(new CountryId('notExisting'))).rejects.toThrowError(CountryNotFoundError)
 
-        const listTariffs = new ListTariffs()
-        await expect(() => listTariffs.execute()).rejects.toThrowError(CountryNotFoundError)
+    })
 
+    it('should return empty when country has not tariff set', async() => {
+        const countryRepository = new InMemoryCountryRepository()
+
+        const country = new Country()
+
+        await countryRepository.save(country)
+
+        const listTariffs = new ListTariffs(countryRepository)
+        const tariffList = await listTariffs.execute(country.id)
+
+        expect(tariffList.length).toEqual(0)
     })
 })
--- src/domain/useCases/ListTariffs.ts
+++ src/domain/useCases/ListTariffs.ts
@@ -1,7 +1,22 @@
 import { CountryNotFoundError } from "@/domain/Errors";
+import type { CountryId } from "@/domain/entities/Country";
+import type { ICountryRepository } from "@/domain/interfaces/ICountryRepository";
+import type { Tariff } from "@/domain/entities/Tariff";
 
 export class ListTariffs {
-    async execute(){
-        throw new CountryNotFoundError()
+    countryRepository: ICountryRepository
+    constructor(countryRepo: ICountryRepository){
+        this.countryRepository = countryRepo
+    }
+
+    async execute(id: CountryId): Promise<Tariff[]>{
+        const country = await this.countryRepository.getById(id)
+
+        if(!country){
+            throw new CountryNotFoundError()
+        }
+
+        return []
+        
     }
 }
